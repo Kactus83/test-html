9c8dee459d1867c3c591032e75ef5fb125e405fe
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-10-03 17:45:25 +0200

    Upgrade CSS stylesheet Project restructuration

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function lancerConfettis(callback) {
     const centerX = rect.left + rect.width / 2;
     const centerY = rect.top + rect.height / 2;
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < 150; i++) {
         let confetti = document.createElement('div');
         confetti.classList.add('confetti');
 
         const angle = Math.random() * 2 * Math.PI;
-        const distance = Math.random() * 150; // Distance max de l'explosion
+        const distance = Math.random() * 250; 
 
         confetti.style.left = (centerX + distance * Math.cos(angle)) + 'px';
         confetti.style.top = (centerY + distance * Math.sin(angle)) + 'px';
@@ -25,14 +25,14 @@ function lancerConfettis(callback) {
 
     // Si une fonction de rappel est fournie, exécutez-la après un délai.
     if (callback) {
-        setTimeout(callback, 5000);
+        setTimeout(callback, 1300);
     }
 }
 
 
 function onEnterButtonClick() {
     lancerConfettis(function() {
-        location.href = 'home.html';
+        location.href = './pages/home/home.html';
     });
 }
 
