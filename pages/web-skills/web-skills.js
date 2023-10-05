5d5c4aa3b2d8a4197c11b63b9f7955e7a1d5c609
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-10-05 22:16:35 +0200

    Web skills page restructuration

--- pages/web-skills/web-skills.js
+++ pages/web-skills/web-skills.js
@@ -12,6 +12,27 @@ function toggleSettings(id) {
 }
 
 /*
+* Listener to change theme and store it in local storage
+*/
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('[data-theme]').forEach(button => {
+        button.addEventListener('click', (e) => {
+            const themeName = e.currentTarget.getAttribute('data-theme');
+            changeTheme(themeName);
+            localStorage.setItem('theme', themeName);
+        });
+    });
+});
+
+/*
+* Usefull change theme immediately (should be imported from main)
+*/
+function changeTheme(themeName) {
+    const root = document.documentElement;
+    root.className = themeName;
+}
+
+/*
 * Particle Cloud with theme's colors (only on canvas)
 */
 
