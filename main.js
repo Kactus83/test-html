b86c14636317744359ad99f6703fe9b299f6a7d7
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2023-10-05 16:23:23 +0200

    Custom cursor Ok

--- main.js
+++ main.js
@@ -6,4 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
 function applyTheme(themeName) {
     const root = document.documentElement;
     root.className = themeName;
-}
+}document.addEventListener('mousemove', (e) => {
+    const cursor = document.getElementById('cursor');
+    cursor.style.left = e.pageX + 'px';
+    cursor.style.top = e.pageY + 'px';
+});
+
+document.querySelectorAll('button').forEach(button => {
+    button.addEventListener('mouseenter', () => {
+        console.log('Button hovered!');
+        document.getElementById('cursor').classList.add('clickable-hover');
+    });
+    button.addEventListener('mouseleave', () => {
+        console.log('Button unhovered!');
+        document.getElementById('cursor').classList.remove('clickable-hover');
+    });
+});
+
