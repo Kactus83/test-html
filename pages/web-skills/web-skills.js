d63f3cf41e89849cf53b3346de2fbbcf6d42f0eb
Florian MORENA <93870523+Kactus83@users.noreply.github.com>
2024-06-17 10:18:21 +0200

    Remove useless script

--- pages/web-skills/web-skills.js
+++ pages/web-skills/web-skills.js
@@ -124,16 +124,3 @@ document.addEventListener('DOMContentLoaded', () => {
     };
     animate();
 });
-
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('.github-project a');
-
-    links.forEach(link => {
-        setInterval(() => {
-            link.classList.add('pulse');
-            setTimeout(() => {
-                link.classList.remove('pulse');
-            }, 1000);
-        }, 10000); // Animation toutes les 10 secondes
-    });
-});
